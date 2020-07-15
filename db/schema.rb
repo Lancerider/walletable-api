7691c987c2d8cfa705bf4f8094394666a237cd86
byteconde <iamdeyvi@gmail.com>
2020-07-15 16:33:35 -0500

    Add migrations and relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_151717) do
+ActiveRecord::Schema.define(version: 2020_07_15_213151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.datetime "date"
+    t.string "payee"
+    t.string "category"
+    t.string "description"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "account_id"
+    t.index ["account_id"], name: "index_transactions_on_account_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -25,4 +45,6 @@ ActiveRecord::Schema.define(version: 2020_07_15_151717) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "accounts", "users"
+  add_foreign_key "transactions", "accounts"
 end
