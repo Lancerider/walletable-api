98fe34ae65822a32000f0a38b3bd6cfb6b3ecc9f
Diego Torres <diegotc86@gmail.com>
2020-07-16 10:16:14 -0500

    Remove account_id from required presence

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ApplicationRecord
   belongs_to :account
   enum category: [:food_and_drinks, :shopping, :services, :transport, :income, :others]
 
-  validates_presence_of :date, :category, :amount, :payee, :account_id
+  validates_presence_of :date, :category, :amount, :payee
   validates_numericality_of :amount
   # validates :category, inclusion: { in: %w(food_and_drinks shopping services transport income others) }
 end
