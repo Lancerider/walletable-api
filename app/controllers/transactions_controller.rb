2409b28ded19519d29b1aea1ca0c8e64dd6afa98
Robert Medina <robertmedib1@gmail.com>
2020-07-20 04:52:48 -0500

    Changin the order

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 class TransactionsController < ApplicationController
-  before_action :set_transaction, only: [:show, :update, :destroy]
+  before_action :set_transaction, only: %i[show update destroy]
 
   def index
-    if (params[:account_id])
+    if params[:account_id]
       @account = current_user.accounts.find(params[:account_id])
       @transactions = @account.transactions
     else
-      @transactions = current_user.transactions
+      @transactions = current_user.transactions.order(date: :desc)
     end
 
     render json: @transactions
@@ -17,21 +19,21 @@ class TransactionsController < ApplicationController
   end
 
   def create
-    if (params[:account_id])
-      @account = current_user.accounts.find_by(id: params[:account_id])
-    else
-      @account = current_user.accounts.first
-    end
-    
-    unless @account
-      render json: {error: "invalid account_id"}, status: :unprocessable_entity
-    else
+    @account = if params[:account_id]
+                 current_user.accounts.find_by(id: params[:account_id])
+               else
+                 current_user.accounts.first
+               end
+
+    if @account
       @transaction = @account.transactions.new(transaction_params)
       if @transaction.save
         render json: @transaction, status: :created, location: @transaction
       else
-        render json: {error: @transaction.errors.full_messages.join(",")}, status: :unprocessable_entity
+        render json: { error: @transaction.errors.full_messages.join(',') }, status: :unprocessable_entity
       end
+    else
+      render json: { error: 'invalid account_id' }, status: :unprocessable_entity
     end
   end
 
@@ -39,7 +41,7 @@ class TransactionsController < ApplicationController
     if @transaction.update(transaction_params)
       render json: @transaction
     else
-      render json: {error: @transaction.errors.full_messages.join(",")}, status: :unprocessable_entity
+      render json: { error: @transaction.errors.full_messages.join(',') }, status: :unprocessable_entity
     end
   end
 
@@ -48,11 +50,12 @@ class TransactionsController < ApplicationController
   end
 
   private
-    def set_transaction
-      @transaction = current_user.transactions.find(params[:id])
-    end
 
-    def transaction_params
-      params.fetch(:transaction, {}).permit(:date,:payee,:category,:description,:amount)
-    end
+  def set_transaction
+    @transaction = current_user.transactions.find(params[:id])
+  end
+
+  def transaction_params
+    params.fetch(:transaction, {}).permit(:date, :payee, :category, :description, :amount)
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Transaction < ApplicationRecord
   belongs_to :account
-  enum category: [:food_and_drinks, :shopping, :services, :transport, :income, :others]
+  enum category: %i[food_and_drinks shopping services transport income others housing]
 
   validates_presence_of :date, :category, :amount, :payee
   validates_numericality_of :amount
